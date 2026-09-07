2222e408e559b3bcf7297f4f2cea4044af61c3c2
Oliveirraz <oliveirabezerraeu@gmail.com>
2026-09-07 02:14:40 -0300

    Atualiza cadastro Aluno

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,22 @@
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import NavBar from "./components/NavBar";
 import CadastroAluno from "./pages/Aluno/CadastroAluno";
 
 function App() {
   return (
     <BrowserRouter>
+      <NavBar />
+
       <Routes>
+        <Route
+          path="/cadastro-aluno"
+          element={<CadastroAluno />}
+        />
 
-        {/* REDIRECT PADRÃO */}
-        <Route path="/" element={<Navigate to="/cadastro-aluno" />} />
-
-        {/* CADASTRO ALUNO */}
-        <Route path="/cadastro-aluno" element={<CadastroAluno />} />
-
+        <Route
+          path="*"
+          element={<Navigate to="/cadastro-aluno" />}
+        />
       </Routes>
     </BrowserRouter>
   );
--- src/pages/Aluno/CadastroAluno.jsx
+++ src/pages/Aluno/CadastroAluno.jsx
@@ -1,9 +1,13 @@
 import "../../assets/css/Aluno/AlunoStyle.css";
-import FormUser from "../../components/Aluno/FormAluno";
+import alunoTela from "../../assets/imagens/cadastroAlunoTela.png";
+import FormUser from "../../components/Aluno/FormUser";
 
 function CadastroAluno() {
   return (
-    <div className="cadastro-aluno-container">
+    <div
+      className="cadastro-aluno-container"
+      style={{ backgroundImage: `url(${alunoTela})` }}
+    >
       <div className="aluno-card">
         <h2 className="aluno-title">Cadastro do Aluno</h2>
 
